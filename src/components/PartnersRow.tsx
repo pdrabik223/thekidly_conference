935149eb79f0ba9a8f218784da63afb4c4cdb093
Piotr Drabik <pdrabik223@gmail.com>
2025-11-05 15:23:05 +0100

    new logos

--- src/components/PartnersRow.tsx
+++ src/components/PartnersRow.tsx
@@ -5,12 +5,11 @@ export const PartnersRow: React.FC = () => {
   const partners = [
     { name: "Kidstation", logo: "public\\logo-kidstation.webp", url: 'https://kidstation.pl/' },
     { name: "TheKIdly", logo: "public\\logo_the_kidly.png", url: 'https://thekidly.com/' },
-    // { name: "Future Systems", logo: "/partner3.png" },
-    // { name: "Digital Solutions", logo: "/partner4.png" },
-    // { name: "Cloud Services", logo: "/partner5.png" },
-    // { name: "Dev Tools Inc", logo: "/partner6.png" },
-    // { name: "AI Research", logo: "/partner7.png" },
-    // { name: "Smart Tech", logo: "/partner8.png" }
+    { name: "25 FRAMES", logo: "public\\Logo-25FRAMES-Gray.png", url: 'https://25frames.pl/' },
+    { name: "#nabieraMYodwagi", logo: "public\\banerodwaga.png", url: 'https://www.umw.edu.pl/pl/nabieramy-odwagi' },
+    { name: "Uniwersytet Medyczny we Wrocławiu", logo: "public\\logo_na_strone_www_75lecie_new.svg", url: 'https://www.umw.edu.pl/pl' },
+    { name: "Random Science", logo: "public\\logo.D88XNlhT_2eszJ6.webp", url: 'https://www.randomscience.org/' },
+    { name: "Connect4Kids", logo: "public\\logo-fundacja-connect4kids.png", url: 'https://fundacja.connect4kids.pl/' },
   ];
 
   // Double the partners array to create seamless scrolling
